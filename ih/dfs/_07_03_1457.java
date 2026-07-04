10ca9d1b444cc6e40b1a5dafc3286fb61bfc8d8f
bosung <qweasdzxc0311@naver.com>
2026-07-04 17:17:11 +0900

    dfs #1457 골드5 수정

--- ih/dfs/_07_03_1457.java
+++ ih/dfs/_07_03_1457.java
@@ -3,64 +3,96 @@ package ih.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /*
-19 40 ->
+20: 05
 
-1이 집이 있는 곳
-0이 집이 없는 곳
+간격이 1
+N * M (100이하)
+K개의 직사각형을 그리면 나머지 부분이 분리된 영역이 된다.
 
-단지 수를 출력하고 단지에 속하는 집의 수를 오름차순으로 정렬
+- 분리된 영역이 몇개이고 각 영역의 넓이가 얼마인지를 구해라
+
+1. map에 칠해진 영역을 구하는 함수 생성 후 map 생성
+2. dfs(bfs)로 순회
+3. dfs(bfs) 순회 결과를 list에 저장해서 출력
+
+dfs, bfs 둘다 구현해보자
 */
 
-public class _07_03_1695 {
+public class _07_03_1457 {
 
-    static int N;
+    static int M,N,K;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {0,0,1,-1};
+    static int[] dx = {0,0,-1,1};
     static int[] dy = {1,-1,0,0};
 
-    static LinkedList<Integer> list = new LinkedList<>();
+    static Deque<int[]> que = new ArrayDeque<>();
     static Stack<int[]> stk = new Stack<>();
+    static LinkedList<Integer> list = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                int num = str.charAt(j) - '0';
-                map[i][j] = num;
-            }
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            makeRectangle(x1,y1,x2,y2);
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(map[i][j] == 1 && !visited[i][j]){
+            for (int j = 0; j < M; j++) {
+                if(map[i][j] == 0 && !visited[i][j]){
                     visited[i][j] = true;
-                    dfs(i,j);
+                    bfs(i,j);
                 }
             }
         }
 
-        Collections.sort(list);
         StringBuilder sb = new StringBuilder();
-
         sb.append(list.size()).append('\n');
+
+        Collections.sort(list);
         for (Integer num : list) {
-            sb.append(num).append('\n');
+            sb.append(num).append(' ');
         }
         System.out.println(sb.toString());
+    }
+
+    public static void bfs(int x, int y){
+        int cnt = 1;
+        que.add(new int[]{x,y});
+
+        while(!que.isEmpty()){
+            int[] nowPos = que.poll();
+            int nowX = nowPos[0];
+            int nowY = nowPos[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+                if(isCan(nextX,nextY)){
+                    visited[nextX][nextY] = true;
+                    que.add(new int[] {nextX,nextY});
+                    cnt++;
+                }
+            }
+        }
+        list.add(cnt);
     }
 
     public static void dfs(int x, int y){
@@ -85,8 +117,16 @@ public class _07_03_1695 {
         list.add(cnt);
     }
 
+    static public void makeRectangle(int x1, int y1, int x2, int y2){
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++) {
+                map[i][j] = 1;
+            }
+        }
+    }
+
     public static boolean isCan(int x, int y){
-        if(x >= 0 && x< N && y >=0 && y<N && map[x][y] == 1 && !visited[x][y]){
+        if(x >= 0 && x< N && y >=0 && y< M && map[x][y] == 0 && !visited[x][y]){
             return true;
         }
         return false;
