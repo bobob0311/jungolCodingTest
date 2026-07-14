336173545e699674b8e0f9a9e1a2d4d6150bb84f
bosung <qweasdzxc0311@naver.com>
2026-07-14 23:39:52 +0900

    add : dp #1407

---
+++ ih/dp/_07_13_1407.java
@@ -0,0 +1,45 @@
+package ih.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+1~34
+*/
+
+public class _07_13_1407 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str = br.readLine();
+        int N = str.length();
+
+        dp = new int[N+1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            // 한 자리 추가의 경우
+            int one = str.charAt(i-1) - '0';
+            if(one>=1 && one<=9){
+                dp[i] += dp[i-1];
+            }
+
+            // 두 자리 추가의 경우
+            if(i>=2){
+                String nowStr = str.substring(i-2,i);
+                int num = Integer.parseInt(nowStr);
+
+                if(num <=34 && num >=10){
+                    dp[i] += dp[i-2];
+                }
+            }
+        }
+        System.out.println(dp[N]);
+    }
+
+}
