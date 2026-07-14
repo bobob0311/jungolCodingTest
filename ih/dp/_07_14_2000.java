527607df5273f20dc721bba7db2d1dce665f0b05
bosung <qweasdzxc0311@naver.com>
2026-07-14 23:40:02 +0900

    add : dp #2000

--- ih/dp/_07_14_2000.java
+++ ih/dp/_07_14_2000.java
@@ -6,40 +6,62 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /*
-1~34
+동전의 단위들이 주어지고 원하는 잔돈이 주어질 때
+잔돈에 맞추기 위해 필요한 최소 동전 수
+동전의 수는 무한하다.
 */
 
 public class _07_14_2000 {
 
-    static int[] dp;
+    static int N,target;
+    static int[] coins,dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        String str = br.readLine();
-        int N = str.length();
 
-        dp = new int[N+1];
-        dp[0] = 1;
+        N = Integer.parseInt(br.readLine());
+        coins = new int[N];
 
-        for (int i = 1; i <= N; i++) {
-            // 한 자리 추가의 경우
-            int one = str.charAt(i-1) - '0';
-            if(one>=1 && one<=9){
-                dp[i] += dp[i-1];
-            }
 
-            // 두 자리 추가의 경우
-            if(i>=2){
-                String nowStr = str.substring(i-2,i);
-                int num = Integer.parseInt(nowStr);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+        }
 
-                if(num <=34 && num >=10){
-                    dp[i] += dp[i-2];
+        target = Integer.parseInt(br.readLine());
+        dp = new int[target+1];
+
+        int answer = sol(target);
+        if(answer == -1){
+            System.out.println("impossible");
+        }else{
+            System.out.println(answer);
+        }
+    }
+
+    static public int sol(int target){
+        for (int i = 1; i <= target ; i++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int coin : coins) {
+                if(i-coin>=0){
+                    int prevDp = dp[i-coin];
+                    if(prevDp == -1) {
+                        continue;
+                    }
+                    min = Math.min(min,dp[i-coin] +1);
                 }
             }
+            if(min == Integer.MAX_VALUE){
+                dp[i] = -1;
+            }else{
+                dp[i] = min;
+            }
         }
-        System.out.println(dp[N]);
+
+        return dp[target];
+
     }
 
 }
