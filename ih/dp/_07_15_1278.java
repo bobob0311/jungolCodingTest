7bfde0dba3f024151ede69812e919ec82c0d7b84
bosung <qweasdzxc0311@naver.com>
2026-07-16 16:42:34 +0900

    add : dp #1278

--- ih/dp/_07_15_1278.java
+++ ih/dp/_07_15_1278.java
@@ -3,65 +3,63 @@ package ih.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /*
-동전의 단위들이 주어지고 원하는 잔돈이 주어질 때
-잔돈에 맞추기 위해 필요한 최소 동전 수
-동전의 수는 무한하다.
+무게 <= W
+
+보석은 한개씩
 */
 
-public class _07_14_2000 {
+public class _07_15_1278 {
 
-    static int N,target;
-    static int[] coins,dp;
+    static int N,W;
+    static int[][] infos;
+    static HashMap<Integer,Integer> map = new HashMap<>();
+    static int[] dp;
 
+
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        coins = new int[N];
-
-
         st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        
+        infos = new int[N][2];
+
         for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            infos[i] = new int[] {w,p};
         }
 
-        target = Integer.parseInt(br.readLine());
-        dp = new int[target+1];
 
-        int answer = sol(target);
-        if(answer == -1){
-            System.out.println("impossible");
-        }else{
-            System.out.println(answer);
-        }
-    }
+        dp = new int[W+1];
 
-    static public int sol(int target){
-        for (int i = 1; i <= target ; i++) {
-            int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int[] nowNode = infos[i];
+            int nowW = nowNode[0];
+            int nowV = nowNode[1];
 
-            for (int coin : coins) {
-                if(i-coin>=0){
-                    int prevDp = dp[i-coin];
-                    if(prevDp == -1) {
-                        continue;
-                    }
-                    min = Math.min(min,dp[i-coin] +1);
+            for (int j = W; j > 0 ; j--) {
+                if(j - nowW > 0 && dp[j-nowW] != 0){
+                    dp[j] = Math.max(dp[j], nowV + dp[j-nowW]);
                 }
             }
-            if(min == Integer.MAX_VALUE){
-                dp[i] = -1;
-            }else{
-                dp[i] = min;
-            }
+            dp[nowW] = Math.max(dp[nowW], nowV);
         }
 
-        return dp[target];
-
+        int answer = 0;
+        for (int i = 0; i <= W; i++) {
+            answer = Math.max(answer,dp[i]);
+        }
+        System.out.println(answer);
     }
 
 }
