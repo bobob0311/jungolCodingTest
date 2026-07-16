9fe10f6afa101bf6db5b9c05ffa73d8ae8facd8e
bosung <qweasdzxc0311@naver.com>
2026-07-16 16:42:53 +0900

    add : dp #1352

--- ih/dp/_07_15_1352.java
+++ ih/dp/_07_15_1352.java
@@ -7,59 +7,57 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /*
-무게 <= W
-
-보석은 한개씩
+양팔저울
+주어진 추만 사용해 구슬의 무게를 확인할 수 있는지를 결정
 */
 
-public class _07_15_1278 {
+public class _07_15_1352 {
 
-    static int N,W;
-    static int[][] infos;
-    static HashMap<Integer,Integer> map = new HashMap<>();
-    static int[] dp;
+    static int N;
+    static boolean dp[];
 
-
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        N = Integer.parseInt(br.readLine());
+
+        dp = new boolean[40001];
+
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        
-        infos = new int[N][2];
-
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-
-            infos[i] = new int[] {w,p};
-        }
-
-
-        dp = new int[W+1];
-
-        for (int i = 0; i < N; i++) {
-            int[] nowNode = infos[i];
-            int nowW = nowNode[0];
-            int nowV = nowNode[1];
-
-            for (int j = W; j > 0 ; j--) {
-                if(j - nowW > 0 && dp[j-nowW] != 0){
-                    dp[j] = Math.max(dp[j], nowV + dp[j-nowW]);
+            for (int j = 40000; j >=0 ; j--) {
+                if(j-w >0 && dp[j-w]){
+                    dp[j] = true;
                 }
             }
-            dp[nowW] = Math.max(dp[nowW], nowV);
+            dp[w] = true;
         }
+        int resultL = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        int answer = 0;
-        for (int i = 0; i <= W; i++) {
-            answer = Math.max(answer,dp[i]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < resultL; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            if(chk(now)){
+                sb.append('Y');
+            }else{
+                sb.append('N');
+            }
+            sb.append(' ');
         }
-        System.out.println(answer);
+        System.out.println(sb.toString());
+    }
+
+    static public boolean chk(int now){
+        if(dp[now]) return true;
+        for (int j = 0; j <= 40000-now ; j++) {
+            if(dp[j] && dp[j + now]){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
